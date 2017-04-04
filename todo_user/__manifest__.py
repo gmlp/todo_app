0d29083bb0bdc78715dd6623345c21d41c756927
gmlp <gmlp.24a@gmail.com>
2017-04-03 20:29:05 -0500

    [ADD]todo_user: Extending Existing Applications **Extends todo app *models     1.Add a field for responsible user     2.Add a field for deadline     3.Add the social network features mail.thread      Abstract class *views     1. Extend form view      1.1 to capture responsible user      1.2 to capture deadline      1.3 to hide active field      1.4 to add social media widget     2. Add responsible field to tree view     3. Extend search view      3.1 as an user I'm able to find by       responsible user.      3.2. I'm able to filter by my tasks and      Not assigned task. ** Modify app data *Alter menu Item name     change it to "My TO-Do" *Add context value to act_window     Add this field to stablish by default my task     filter on tree view. *Modify record rules     Set the domain force to make a task visible     to the responsible user or to everyone if the responsible     user is not set.     Also It is visible by followers.

---
+++ todo_user/__manifest__.py
@@ -0,0 +1,13 @@
+{  'name': 'Multiuser To-Do',
+    'description': 'Extend the To-Do app to multiuser.',
+    'author': 'Gonzalo Lopez',
+    'depends': [
+        'todo_app',
+        'mail',
+    ],
+    'data': [
+        'views/todo_task.xml',
+        'security/todo_access_rules.xml',
+    ],
+ }
+
