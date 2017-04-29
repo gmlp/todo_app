79ecbb4f42133e3d27fd1a433cb692523ea5e8f7
gmlp <gmlp.24a@gmail.com>
2017-04-28 20:02:02 -0500

    [FIX]Todo_ui: wizard  wrong user reference

--- todo_ui/wizards/todo_wizard_model.py
+++ todo_ui/wizards/todo_wizard_model.py
@@ -24,38 +24,39 @@ class TodoWizard(models.TransientModel):
         if self.new_deadline:
             vals['date_deadline'] = self.new_deadline
         if self.new_user_id:
-            vals['user_id'] = self.new_user_id
+            vals['user_id'] = self.new_user_id.id
         # Mass write values on all selected tasks
         if vals:
             self.task_ids.write(vals)
         return True
 
 
-  #  @api.multi
-  #  def do_count_tasks(self):
-  #      Task = self.env['todo.task']
-  #      count = Task.search_count([('is done', '=', False)])
-  #      raise exceptions.Warning(
-  #          'There are %d active tasks.' %count
-  #          )
+    @api.multi
+    def do_count_tasks(self):
+        Task = self.env['todo.task']
+        count = Task.search_count([('is_done', '=', False)])
+        raise exceptions.Warning(
+            'There are %d active tasks.' %count
+            )
 
-  #  @api.multi
-  #  def _reopen_form(self):
-  #      self.ensure_one()
-  #      return {
-  #          'type': 'ir.actions.act_window',
-  #          'res_model': self._name,  # this model
-  #          'res_id': self.id,  # the current wizard record
-  #          'view_type': 'form',
-  #          'view_mode': 'form',
-  #          'target': 'new'
-  #      }
-  #  @api.multi
-  #  def do_populate_tasks(self):
-  #      self.ensure_one()
-  #      Task = self.env['todo.task']
-  #      open_tasks = Task.search([('is_done', '=', False)])
-  #      # Fill the wizard Task list with all tasks
-  #      self.task_ids = all_tasks
-  #      # reopen wizard form on same wizard record
-  #      return self._reopen_form()
+    @api.multi
+    def _reopen_form(self):
+        self.ensure_one()
+        return {
+            'type': 'ir.actions.act_window',
+            'res_model': self._name,  # this model
+            'res_id': self.id,  # the current wizard record
+            'view_type': 'form',
+            'view_mode': 'form',
+            'target': 'new'
+        }
+
+    @api.multi
+    def do_populate_tasks(self):
+        self.ensure_one()
+        Task = self.env['todo.task']
+        open_tasks = Task.search([('is_done', '=', False)])
+        # Fill the wizard Task list with all tasks
+        self.task_ids = open_tasks
+        # reopen wizard form on same wizard record
+        return self._reopen_form()
