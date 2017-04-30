325006cc17d7c8143f6090e7b6d9e2e40599a5e3
gmlp <gmlp.24a@gmail.com>
2017-04-29 19:22:19 -0500

    [ADD]todo_kanban: kanban view for TO-do

---
+++ todo_kanban/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': 'To-Do Kanban',
+    'description': 'Kanban board for to-do tasks.',
+    'author': 'Gonzalo Lopez',
+    'depends': ['todo_ui'],
+    'data': [
+        'views/todo_view.xml',
+    ]
+}
