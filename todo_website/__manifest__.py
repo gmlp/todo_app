a36098ab866dadc287ddd2fb24eb15ed76a95af6
gmlp <gmlp.24a@gmail.com>
2017-05-09 13:30:35 -0500

    [ADD]todo_website

---
+++ todo_website/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'User interface improvements to the To-Do app',
+    'description': 'User friendly features.',
+    'author': 'Gonzalo Lopez',
+    'depends': [
+        'todo_kanban',
+        'website_form',
+    ],
+    'data': [
+        'data/config_data.xml',
+        'views/todo_templates.xml',
+        'views/todo_web.xml',
+        'views/todo_views.xml',
+        'views/todo_extend.xml',
+    ]
+}
---
+++ todo_website/controllers/main.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from odoo import http
+from odoo.http import request
+
+class Todo(http.Controller):
+
+    @http.route('/helloworld', auth='public')
+    def hello_world(self):
+        return('<h1>Hello World!</h1>')
+
+    @http.route('/hello', auth='public', website=True)
+    def hello(self):
+        return request.render('todo_website.hello')
+
+    @http.route('/todo', auth='user' , website=True)
+    def index(self, **kwargs):
+        TodoTask = request.env['todo.task']
+        tasks =  TodoTask.search([])
+        return request.render(
+            'todo_website.index', {'tasks': tasks})
+
+    @http.route('/todo/<model("todo.task"):task>', website=True)
+    def showTask(self, task, **kwargs):
+        return http.request.render(
+            'todo_website.detail',
+            {'task': task})
+
+    @http.route('/todo/add', website=True)
+    def add(self, **kwargs):
+            users = request.env['res.users'].search([])
+            return request.render(
+                'todo_website.add', {'users': users})
